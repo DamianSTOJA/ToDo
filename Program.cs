d205f180dea5de4e9d5cf4e11a7390df2406afe2
damo <hello@weemind.app>
2025-03-06 15:46:39 +0100

    The server runs with one route.

--- Program.cs
+++ Program.cs
@@ -4,22 +4,26 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+
 builder.Services.AddEndpointsApiExplorer();
 
-// Configure the HTTP request pipeline.
+builder.Services.AddSwaggerGen(c =>
+  {
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Todo API", Description = "Keep track of your tasks", Version = "v1" });
+  });
+
+var app = builder.Build();
+
 if (app.Environment.IsDevelopment())
 {
-  builder.Services.AddSwaggerGen(c =>
-    {
-      c.SwaggerDoc("v1", new OpenApiInfo { Title = "Todo API", Description = "Keep track of your tasks", Version = "v1" });
-    });
+  app.UseSwagger();
+  app.UseSwaggerUI(c =>
+  {
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Todo API V1");
+  });
 }
-app.UseSwaggerUI(c =>
-{
-  c.SwaggerEndpoint("/swagger/v1/swagger.json", "Todo API V1");
-});
-}
-
+// Configure the HTTP request pipeline.
 app.UseHttpsRedirection();
 
 app.MapGet("/", () =>
